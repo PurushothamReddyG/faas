3604a753175bd75b7d747c8e4212e6e0b03a8117
PurushothamReddy <32892297+PurushothamReddyG@users.noreply.github.com>
2017-12-14 12:34:03 +0530

    Update index.js

--- faas-edp/index.js
+++ faas-edp/index.js
@@ -1,11 +1,11 @@
 exports.test = function test(req, res) {
-  // Example input: {"message": "Hello!"}
-  if (req.body.message === undefined) {
+  // Example input: {"name": "Hello!"}
+  if (req.body.name === undefined) {
     // This is an error case, as "message" is required.
     res.status(400).send('Welocme to FaaS World !');
   } else {
     // Everything is okay.
-    console.log(req.body.message);
-    res.status(200).send('Hello Mr: ' + req.body.message +', Welcome to Faas world !');
+    console.log(req.body.name);
+    res.status(200).send('Hello Mr: ' + req.body.name +', Welcome to Faas world !');
   }
 };
